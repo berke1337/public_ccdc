5f53421c729421ca3f609e37102fc37b0380ccf0
Tobias Boelter <t0b@t0b.org>
2017-02-04 12:46:48 -0800

    working and enhanced version of chpasswd.sh

--- chpasswd.sh
+++ chpasswd.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
-# setpass
 
 passfile=passwords.txt
-passfile=passwords.txt
->$passfile
+passfile_csv=passwords.csv
 
 list="alpha bravo charlie"
+list=`awk -F':' '{ if ( $7 !~ "nologin" && $7 !~ "false" ) print $1 }' "/etc/passwd"`
 
-list=awk -F':' '{ if ( $7 !~ "nologin" && $7 !~ "false" ) print $1 }' "/etc/passwd"
+echo "" > $passfile
+echo "" > $passfile_csv
 
 for user in $list
- do
-   if  ! grep -w ^$user /etc/passwd > /dev/null
-   then
-    echo "user NOT present: $user"
-  else
-   echo "user present: $user"
-  pass=$(pwgen 16 1)
-  echo "$user:$pass">>$passfile
- fi
+  do
+    if  ! grep -w ^$user /etc/passwd > /dev/null
+    then
+      echo "user NOT present: $user"
+    else
+      read -p "Include $user in password change list? [y/n]" -n 1 -r
+      echo ""
+      if [[ $REPLY =~ ^[Yy]$ ]]
+      then
+        pass=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c12; echo "")
+        echo "$user:$pass">>$passfile
+        echo "$user,$pass">>$passfile_csv
+      fi
+    fi
 done
 cat $passfile | chpasswd
