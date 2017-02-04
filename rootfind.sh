e48bf37d9233d8ec2a6188e077df23c20847a4c4
Anders Lewis <greagle3@gmail.com>
2017-02-04 15:34:42 -0800

    Add rootfind.sh

---
+++ rootfind.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# combs through /etc/
+dirnames=("/etc/apache" "/etc/httpd" "/etc/lighttpd" "/etc/nginx")
+keywords=("Alias" "Location" "root")
+logname="./webroots.txt"
+rm $logname
+touch $logname
+
+for d in "${dirnames[@]}"; do
+	echo "checking for $d"
+	printf "***$d:***\n\n" >> $logname
+	if [ -d $d ]; then
+		echo "Found directory for $d"
+		echo "Found directory $d" >> $logname
+		for kw in "${keywords[@]}"; do
+			echo "Grepping through for $kw"
+			printf "\n  **grep results for $kw:\n" >> $logname
+			grep -A1 -r $kw $d | sed 's/^/    /g' >> $logname
+		done
+	else
+		echo "No dir found for $d" >> $logname
+	fi
+	printf "\n\n" >> $logname
+done;
+
+echo
+echo "Search for *** for application delimeters"
+echo "Search for ** for specific grep keywords."
